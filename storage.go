4eeb90cdc867f054bd4b9b917c976ec75fe717f0
Victor Uche <ucheviquetor@gmail.com>
2026-08-13 00:20:32 +0100

    feat: implement storage functions

---
+++ storage.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Loads tasks from a file with the given filename. Missing file means empty task list
+func LoadTasks(filename string) ([]Task, error) {
+	data, err := os.ReadFile(filename)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []Task{}, nil
+		}
+		return nil, fmt.Errorf("read tasks from file %q: %w", filename, err)
+	}
+
+	tasks := []Task{}
+	if err := json.Unmarshal(data, &tasks); err != nil {
+		return nil, fmt.Errorf("decode tasks from file %q: %w", filename, err)
+	}
+
+	if tasks == nil {
+		tasks = []Task{}
+	}
+
+	return tasks, nil
+}
+
+// Saves tasks to a file
+func SaveTasks(filename string, tasks []Task) error {
+	data, err := json.MarshalIndent(tasks, "", " ")
+
+	if err != nil {
+		return fmt.Errorf("encode tasks %w", err)
+	}
+
+	data = append(data, '\n')
+
+	if err := os.WriteFile(filename, data, 0644); err != nil {
+		return fmt.Errorf("write tasks to file %q: %w", filename, err)
+	}
+
+	return nil
+}
